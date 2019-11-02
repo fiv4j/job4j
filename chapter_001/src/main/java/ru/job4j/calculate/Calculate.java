7081fc38a0e43de53a8975e3d7e47b64c3cb9fd0
fiv4j <ifrolov@rambler.ru>
2019-11-02 19:15:41 +0300

    Коммит к задаче 5.8. JavaDoc. Code convention[#194005]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculate;
+
+/**
+ * Класс Calculate для вычисления арифметических операций: + - * /
+ * @author fiv4j
+ * @since 02.11.19
+ * @version 1
+ */
+public class Calculate {
+    /**
+     * Main. Метод для демонстрвции работы основных методов класса.
+     * @param args - аргументы командной строки.
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Ilya Frolov (ifrolov@rambler.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
