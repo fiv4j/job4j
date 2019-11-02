cd5a8b3a4b55554c03ae66fe8272736d788f6cdc
fiv4j <ifrolov@rambler.ru>
2019-11-02 18:20:59 +0300

    Коммит с исправлениями к задаче 5.6. Простые числа.[#194008]

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -2,18 +2,10 @@ package ru.job4j.loop;
 
 public class PrimeNumber {
     public int calc(int finish) {
-        int count = finish;
-        for (int num = 1; num <= finish; num++) {
-            if (num == 1) {
-                count--;
-                continue;
-            }
-            for (int i = 2; i < num / 2 + 1; i++) {
-                if (num % i == 0) {
-                    count--;
-                    break;
-                }
-            }
+        int count = 0;
+        CheckPrimeNumber isPrime = new CheckPrimeNumber();
+        for (int i = 1; i <= finish; i++) {
+            count += isPrime.check(i) ? 1 : 0;
         }
         return count;
     }
