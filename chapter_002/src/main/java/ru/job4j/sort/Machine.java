24346cbdfd5c5ad9636c44019bf8e61ec2a9e9e1
fiv4j <ifrolov@rambler.ru>
2019-11-19 14:50:34 +0300

    Fix for checkstyle.

--- chapter_002/src/main/java/ru/job4j/sort/Machine.java
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -3,16 +3,16 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int changeOfMoney = money - price;
-        for (int i = 0; changeOfMoney > 0 && i < COINS.length; i++) {
-            while (changeOfMoney >= COINS[i]) {
-                changeOfMoney -= COINS[i];
-                rsl[size++] = COINS[i];
+        for (int i = 0; changeOfMoney > 0 && i < coins.length; i++) {
+            while (changeOfMoney >= coins[i]) {
+                changeOfMoney -= coins[i];
+                rsl[size++] = coins[i];
             }
         }
 
