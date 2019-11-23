7e71488f2da420a6f7d7ab6f27a18cd9b03fa416
fiv4j <ifrolov@rambler.ru>
2019-11-23 10:54:44 +0300

    Исправления к задаче 3. Компаратор для строк. [#194076]

--- chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -9,7 +9,7 @@ public class ListCompare implements Comparator<String> {
         int result = 0;
         int minLength = Math.min(s.length(), t1.length());
         for (int idx = 0; result == 0 && idx < minLength; idx++) {
-            result += Character.compare(s.charAt(idx), t1.charAt(idx));
+            result = Character.compare(s.charAt(idx), t1.charAt(idx));
         }
         if (result == 0 && s.length() != t1.length()) {
             result = s.length() > t1.length() ? 1 : -1;
